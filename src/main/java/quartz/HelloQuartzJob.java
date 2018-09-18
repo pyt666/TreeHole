3cdf62d377c26168cff4d17f2b443782f9a6b603
pyt666 <2630451673@qq.com>
2018-09-18 14:54:11 +0800

    quartz简单实现

--- src/main/java/quartz/HelloQuartzJob.java
+++ src/main/java/quartz/HelloQuartzJob.java
@@ -1,17 +1,45 @@
 package quartz;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
+import org.quartz.CronScheduleBuilder;
 import org.quartz.Job;
+import org.quartz.JobBuilder;
+import org.quartz.JobDataMap;
+import org.quartz.JobDetail;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
+import org.quartz.Scheduler;
+import org.quartz.SchedulerException;
+import org.quartz.SchedulerFactory;
+import org.quartz.Trigger;
+import org.quartz.TriggerBuilder;
+import org.quartz.impl.StdSchedulerFactory;
 
 public class HelloQuartzJob implements Job {
+	
+
+	public HelloQuartzJob() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
 
 	public void execute(JobExecutionContext context) 
             throws JobExecutionException {
-        System.out.println("Hello, Quartz! - executing its JOB at "+ 
-            new Date() + " by " + context.getTrigger().getName());
+        String instName = context.getJobDetail().getKey().getName();
+        String groupName = context.getJobDetail().getKey().getGroup();
+        JobDataMap dataMap = context.getJobDetail().getJobDataMap();
+        String myDescription = dataMap.getString("myDescription");
+        int myValue = dataMap.getInt("myValue");
+        List<String> myArray = (List<String>) dataMap.get("myArray");
+        System.out.println("---> Instance = " + instName + ", group = " + groupName
+                + ", description = " + myDescription + ", value =" + myValue
+                + ", array item[0] = " + myArray.get(0));
+        System.out.println("Runtime: " + new Date().toString() + " <---");
     }
+	
+	
 
 }
--- src/main/java/utils/HelloQuartzScheduling.java
+++ src/main/java/utils/HelloQuartzScheduling.java
@@ -1,33 +1,45 @@
 package utils;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import org.quartz.CronScheduleBuilder;
+import org.quartz.JobBuilder;
 import org.quartz.JobDetail;
 import org.quartz.Scheduler;
 import org.quartz.SchedulerException;
 import org.quartz.SchedulerFactory;
 import org.quartz.SimpleTrigger;
+import org.quartz.Trigger;
+import org.quartz.TriggerBuilder;
 import org.quartz.impl.StdSchedulerFactory;
 
 import quartz.HelloQuartzJob;
 public class HelloQuartzScheduling {
 
-    public static void main(String[] args)throws SchedulerException {
-
-        SchedulerFactory schedulerFactory = new StdSchedulerFactory();
-        Scheduler scheduler = schedulerFactory.getScheduler();
-
-        JobDetail jobDetail = new JobDetail("helloQuartzJob", 
-                Scheduler.DEFAULT_GROUP, HelloQuartzJob.class);
-
-        SimpleTrigger simpleTrigger = new SimpleTrigger("simpleTrigger", 
-                Scheduler.DEFAULT_GROUP);
-
-        simpleTrigger.setStartTime(new Date(System.currentTimeMillis()));
-        simpleTrigger.setRepeatInterval(5000);
-        simpleTrigger.setRepeatCount(10);
-
-        scheduler.scheduleJob(jobDetail, simpleTrigger);
-
-        scheduler.start();
-    }
-
+	public static void main(String[] args) throws SchedulerException, InterruptedException {
+		System.out.println("currentTime:" + new Date());
+		SchedulerFactory sf = new StdSchedulerFactory();
+		Scheduler scheduler = sf.getScheduler();
+		
+		JobDetail job = JobBuilder.newJob(HelloQuartzJob.class).withIdentity("job1", "group1").build();
+		
+		Trigger trigger = TriggerBuilder.newTrigger()
+				.withIdentity("trigger1", "group1")
+				.withSchedule(CronScheduleBuilder.cronSchedule("*/5 * * * * ?"))
+				.build();
+		
+		job.getJobDataMap().put("myDescription", "Hello Quartz");
+		job.getJobDataMap().put("myValue", 1990);
+		List<String> list = new ArrayList<>();
+		list.add("firstItem");
+		job.getJobDataMap().put("myArray", list);
+		
+		scheduler.scheduleJob(job,trigger);
+		scheduler.start();
+		Thread.sleep(7000);
+		scheduler.shutdown();
+		System.out.println("currentTime:" + new Date());
+	}
 }
